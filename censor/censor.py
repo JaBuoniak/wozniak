c368d39e31ee0bf1d0873a5292f027409f158509
Paweł Jabłoński <pjablonski.elk@gmail.com>
2021-03-14 20:06:27 +0100

    Wspólna wersja z pociągu

--- censor/censor.py
+++ censor/censor.py
@@ -1,12 +1,20 @@
 # Opiszmy co tu się wyrabia:
 def censor(text, word):  # definiujesz funkcję, która ma ocenzurować wszystkie wystąpienia słowa 'word' w tekście 'text'
-    for char in text:   # przechodzisz po wszystkich znakach zadanego tekstu 'text'
-        x = text.lower()  # 'x' staje się tekstem 'text' zapisanym tylko małymi literami, ale to nie istotne, bo nie używasz tej zmiennej
-        if word == "posiada":   # sprawdzasz, czy słowo 'word', które podałaś dla funkcji ma wartość 'posiada' - w tym przypadku zawsze jest to prawda
-            for char in word:   # jeśli to prawda, to dla każdej litery w słowie 'word' (czyli dla liter 'p', 'o', 's', 'i', 'a', 'd', 'a') wykonujesz co następuje:
-                new = replace(char, "*")    # zmienna 'new' dostaje wartość, jaką zwróci funkcja 'replace(c,r)' - na dobrą sprawę nie wiem, czego oczekujesz, bo znalazłem kilka zupełnie różnych implementacji tej funkcji
-    print(new)  # po przejściu wszystkich liter zadanego tekstu 'text' wypisuje ostatni wynik funkcji 'replace', czyli 'replace("a", "*")'
-# no i tyle. Funkcja 'censor' kończy się ale nic nie zwraca.
+    censoredText = ""
+    wordLength = len(word)
+    positionOfFirstFoundWord = text.find(word)
+    actualPosition = 0
+    while(actualPosition < len(text)):
+
+        if actualPosition >= positionOfFirstFoundWord and actualPosition < positionOfFirstFoundWord+wordLength:
+            censoredText += "*"
+        else:
+            censoredText += text[actualPosition]
+
+        actualPosition += 1
+
+    return censoredText
+
 
 zdanie = "Ania posiada kota"
 slowo = "posiada"
